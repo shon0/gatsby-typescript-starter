e362209519d0fa1cc94bf9c82f6791103b1f5b42
shono-k <shono.koga@nextinnovation-inc.co.jp>
2019-12-25 19:22:42 +0900

    Set alias

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import Header from "src/components/header"
+import "src/components/layout.css"
 
 const Layout: React.FC = ({ children }) => {
   const data = useStaticQuery(graphql`
