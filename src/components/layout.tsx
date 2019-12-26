a50ebc84d82c67c04a9648f498e2b747c1f2bab3
shono-k <shono.koga@nextinnovation-inc.co.jp>
2019-12-27 05:16:53 +0900

    Add eslint

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -28,8 +28,7 @@ const Layout: React.FC = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
+          {`©${new Date().getFullYear()}, Built with`}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
